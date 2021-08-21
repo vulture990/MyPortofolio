03e7cf45dc41280d70ad48b1a61e4bb1a37e0585
unknown <mohamedsali602@live.com>
2021-08-21 14:39:53 +0200

    logo component done

---
+++ portofolio/mywebsite/pages/index.js
@@ -0,0 +1,13 @@
+import Logo from '../components/Logo'
+import MetaTags from '../components/MetaTags'
+
+export default function Home() {
+  return (
+    <div>
+      <MetaTags 
+        title="Salbi Mohamed -Software Engineer, Creator"
+      />
+      <Logo/>
+    </div>
+  )
+}
