5dd65751abf38507035a477bbd132d76eb892993
unknown <mohamedsali602@live.com>
2021-08-22 19:01:38 +0200

    for my sanity pls deploy

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+declare namespace webpack {
+    export type Compiler = any
+    export type Plugin = any
+  }
+  declare module "next/dist/compiled/webpack/webpack" {
+    export const webpack: any
+  }
 // / <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
